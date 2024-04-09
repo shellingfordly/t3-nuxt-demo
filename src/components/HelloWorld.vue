1f42f8e2cb64e2a3dbf1d1e52c6a5112f92c06f4
shellingfordly <shellingfordly@qq.com>
2024-04-09 14:38:54 +0800

    feat: add vue-clerk

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,12 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { SignInButton, UserButton, useUser } from "vue-clerk";
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+const user = useUser();
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <SignInButton v-if="!user.isSignedIn" />
+  <UserButton v-else />
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import { clerkPlugin } from "vue-clerk/plugin";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(clerkPlugin, {
+  publishableKey: import.meta.env.VITE_CLERK_PUBLISHABLE_KEY,
+});
+
+app.mount("#app");
