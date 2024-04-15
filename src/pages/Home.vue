82ec1ef5173fdb5c3f0d9733ee2d832f182f78bd
shellingfordly <shellingfordly@qq.com>
2024-04-15 15:17:29 +0800

    feat: add login view comments tips

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,10 +14,17 @@ const showMore = computed(
   <!-- <HelloWorld /> -->
   <div container ma pb10 lt-sm:p5>
     <div p5 space-x-4>
-      <span class="hc-gray i-material-symbols:light-mode dark:i-material-symbols:dark-mode" @click="isDark = !isDark"
-        title="change theme" />
-      <a href="https://github.com/shellingfordly/vue-comment" target="_blank" class="hc-gray i-mdi:github"
-        title="github" />
+      <span
+        class="hc-gray i-material-symbols:light-mode dark:i-material-symbols:dark-mode"
+        @click="isDark = !isDark"
+        title="change theme"
+      />
+      <a
+        href="https://github.com/shellingfordly/vue-comment"
+        target="_blank"
+        class="hc-gray i-mdi:github"
+        title="github"
+      />
     </div>
 
     <div flex-center-between py2 mb3 b-b-default b-b-1>
@@ -26,17 +33,35 @@ const showMore = computed(
       </div>
       <div>
         Powered by
-        <a class="c-blue dark:c-blue-700 a-blue" href="https://github.com/shellingfordly/" target="_blank">
+        <a
+          class="c-blue dark:c-blue-700 a-blue"
+          href="https://github.com/shellingfordly/"
+          target="_blank"
+        >
           GitHub
         </a>
         &
-        <a class="c-blue dark:c-blue-700 a-blue" href="https://github.com/shellingfordly/vue-comment" target="_blank">
+        <a
+          class="c-blue dark:c-blue-700 a-blue"
+          href="https://github.com/shellingfordly/vue-comment"
+          target="_blank"
+        >
           vue-comment
         </a>
       </div>
     </div>
     <CommentHeader />
-    <CommentList />
+    <CommentList v-if="githubCommentStore.isAuthed" />
+    <div
+      v-else
+      py5
+      text-center
+      underline
+      cursor-pointer
+      @click="githubCommentStore.loginAuthorize"
+    >
+      Login View Comments
+    </div>
     <div v-if="showMore" flex-center-center p5>
       <button p2 c-gray a-blue @click="githubCommentStore.updateComments">
         <span>VIEW MORE</span>
